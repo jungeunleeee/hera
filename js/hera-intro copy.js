487fdcf9e9ca5a9b008ca7e0d6f26859cd92c466
jungeunleeee <blossomeuni@naver.com>
2022-09-23 18:28:15 +0900

    hera navigation진행중

--- js/hera-intro copy.js
+++ js/hera-intro copy.js
@@ -5,11 +5,12 @@ $(document).ready(function () {
   setImageSlide('#main-visual div.image-box', 1, true, 5000);
   setBannerSlide('div.banner-slide:eq(0)', 1);
   navigation();
+  navigationMobile();
   footerToggleButton();
   footerDropdown();
   footerLanguage();
   resizeWindow();
-
+ 
 });
 
 function preventDefaultAnchor() {
@@ -269,163 +270,126 @@ function setBannerSlide(selector, first) {
 
 function navigation() {
   var windowWidth = $(window).width();
+  console.log(windowWidth);
   if (windowWidth >= 1024) {
-    // pc버전일때 index구하는 법
     document.querySelectorAll('#nav > ul > li > a').forEach(function (el, i) {
+
+      // event: mouseover
       el.addEventListener('mouseover', function () {
+        // 변수 선언
         var index = Array.from(document.querySelectorAll('#nav > ul > li > a')).indexOf(this);
         console.log(index);
+
+        // 이벤트1 : 해당하는 box보여주기
+        document.querySelectorAll('#nav > ul > li').forEach(function (el, i) {
+          el.classList.remove('on');
+        });
+        document.querySelector('#nav > ul > li:nth-child(' + (index + 1) + ')').classList.add('on');
+        // header의 높이 줄이기
+        document.querySelector('header#header').classList.add('open');
+      });
+
+      // // event: mouseleave :: 마우스가 nav tag밖으로 나가면 취소
+      document.querySelector('#nav').addEventListener('mouseleave', function () {
+        document.querySelectorAll('#nav > ul > li').forEach(function (el, i) {
+          el.classList.remove('on');
+        });
+        // header 그대로
+        document.querySelector('header#header').classList.remove('open');
       });
     });
   } else {
-    // mobile버전일때 index구하는 법
-    document.querySelectorAll('#nav > ul > li > a').forEach(function (el, i) {
-      el.addEventListener('click', function (e) {
-        e.preventDefault();
-        // 인덱스 구하기
-        var index = Array.from(document.querySelectorAll('#nav > ul > li > a')).indexOf(this);
-        console.log(index + '지금 번호 알려줘');
-        console.log(index);
-        var sumBoxHeight = 0;
-        var sumFigureHeight;
-        // var sumFigureHeight = $('#nav div.figure').outerHeight(true);
+    return false;
+  }
+}
 
-        $('#nav > ul > li:eq(' + index + ') div.box ul > li > a').each(function () {
-          sumBoxHeight = $('#nav > ul > li:eq(' + index + ') > div.box > div.nav-box').outerHeight(true);
-          console.log(sumBoxHeight + '/' + sumFigureHeight);
-          if (sumFigureHeight === undefined) {
-            sumFigureHeight = 0;
-          } else {
-            sumFigureHeight = $('#nav > ul > li:eq(' + index + ') > div.box > div.figure').outerHeight(true);
-          }
-        });
-        $('#nav > ul > li div.box').css({
-          'height': 0,
-          'transition': 'height 0.5s'
-        });
-        $('#nav > ul > li:eq(' + index + ') div.box').css({
-          'height': sumBoxHeight + sumFigureHeight
-        });
+function navigationMobile() {
+  var windowWidth = $(window).width();
+  if (windowWidth < 1024) {
+    $('#hamburger-button').on('click', function(){
+      $('#nav').css({'right' : "0"});
+    });
+    $('#list-mobile > li:eq(2) > a').on('click', function() {
+      $('#nav').css({'right' : "-100%"});
+    });
+  } else {
+    return false;
+  }
+}
 
-        if ($('#nav > ul > li:eq(' + index + ') div.box').outerHeight(true) > 0) {
-          $('#nav > ul > li:eq(' + index + ') div.box').css({
-            'height': 0
-          });
-        }
+function navigationMobile() {
+  var windowWidth = $(window).width();
+  if (windowWidth < 1024) {
+    // 모바일버전 햄버거버튼 클릭했을 때 창 나타나기
+    $('#hamburger-button').on('click', function () {
+      $('#nav').addClass('on');
+    });
+    $('#list-mobile > li:eq(2) > a').on('click', function () {
+      $('#nav').removeClass('on');
+    });
 
-        // 2번째 창 열기
-        // ul.list > li 합 구야함.
-        $('#nav > ul > li:eq('+ index +') ul.sub-nav > li > a').on('click', function(){
-          var idx = $('#nav > ul > li:eq('+ index +') ul.sub-nav > li').index($(this).parent());
-          console.log(idx);
-          var sumLiHeight = 0;
-          $('#nav > ul > li:eq('+ index+') ul.sub-nav > li:eq('+ idx +') ul.list > li ').each(function() {
-            sumLiHeight += $(this).outerHeight(true);
-          });
-          console.log(sumLiHeight + '왜 ㅇ안나와');
+    // 눌렀을 때 자기꺼 펼쳐지기
+    $('#nav > ul > li > a').on('click', function (e) {
+      e.preventDefault();
+      var index = $('#nav > ul > li').index($(this).parent());
+      var sumBoxHeight = 0;
+      var sumFigureHeight = $('#nav div.figure').outerHeight(true);
+      console.log(sumFigureHeight + 'dddd') 
 
-          $('#nav > ul > li  ul.sub-nav ul.list').css({
-            'height' : 0,
-            'transition' : 'height 0.5s'
-          });
+      $('#nav > ul > li:eq(' + index + ') div.box ul > li > a').each(function () {
+        sumBoxHeight += $(this).outerHeight(true);
+      });
+      $('#nav > ul > li  div.box').css({
+        'height': 0,
+        'transition': 'height 0.5s'
+      })
 
-          $('#nav > ul > li:eq('+ index +') ul.sub-nav > li:eq('+idx +') ul.list').css({
-            'height' : sumLiHeight
-          });
+      $('#nav > ul > li:eq(' + index + ') div.box').css({
+        'height': sumBoxHeight + sumFigureHeight
+      });
+    });
 
-          $('#nav > ul > li:eq(' + index + ') div.box').css({
-            'height': sumBoxHeight + sumFigureHeight + sumLiHeight
-          });
+    // 2번째 창
+    $('#nav div.nav-box  ul.sub-nav > li > a').on('click', function () {
+      var index = $('#nav div.nav-box ul.sub-nav > li').index($(this).parent());
 
-
-
-        });
-        // document.querySelectorAll('#nav div.nav-box ul.sub-nav > li > a').forEach(function (el, i) {
-        //   el.addEventListener('click', function (e) {
-        //     e.preventDefault();
-        //     var index = Array.from(document.querySelectorAll('#nav > ul > li div.box > div.nav-box ul.sub-nav > li:eq('+ index +') ')).indexOf(this);
-        //     console.log(index + ': small')
-        //   });
-        // });
-
-
-
-
+      var sumHeight = 0;
+      $('#nav div.nav-box  ul.sub-nav > li:eq(' + index + ') > ul.list > li > a').each(function () {
+        sumHeight += $(this).outerHeight(true);
       });
 
-      // mobile nav 열기
-      $('#hamburger-button').on('click', function () {
-        $('#nav').addClass('on');
+      $('#nav div.nav-box  ul.sub-nav > li > ul.list').css({
+        'height': 0,
+        'transition': 'height 0.5s'
       });
 
-      // mobile nav 닫기
-      $('#nav > div.header.mobile ul > li:eq(2) > a').on('click', function () {
-        $('#nav').removeClass('on');
+      $('#nav div.nav-box  ul.sub-nav > li:eq(' + index + ') > ul.list').css({
+        'height': sumHeight
       });
-
-
-
-
     });
 
 
 
+    // resize되었을 때는 창이 닫혀야 함.  
+  } else {
+    return false;
   }
-
-
-
-
-
-
-  // if (windowWidth >= 1024) {
-  //   document.querySelectorAll('#nav > ul > li > a').forEach(function (el, i) {
-
-  //     // event: mouseover
-  //     el.addEventListener('mouseover', function () {
-  //       // 변수 선언
-
-  //       var index = Array.from(document.querySelectorAll('#nav > ul > li > a')).indexOf(this);
-  //       console.log(index);
-
-
-  //       // 이벤트1 : 해당하는 box보여주기
-  //       document.querySelectorAll('#nav > ul > li').forEach(function (el, i) {
-  //         el.classList.remove('on');
-  //       });
-  //       document.querySelector('#nav > ul > li:nth-child(' + (index + 1) + ')').classList.add('on');
-  //       // header의 높이 줄이기
-  //       document.querySelector('header#header').classList.add('open');
-  //     });
-
-  //     // // event: mouseleave :: 마우스가 nav tag밖으로 나가면 취소
-  //     document.querySelector('#nav').addEventListener('mouseleave', function () {
-  //       document.querySelectorAll('#nav > ul > li').forEach(function (el, i) {
-  //         el.classList.remove('on');
-  //       });
-  //       // header 그대로
-  //       document.querySelector('header#header').classList.remove('open');
-  //     });
-  //   });
-  // } else {
-  //   // mobile nav 열기
-  //   $('#hamburger-button').on('click', function(){
-  //     $('#nav').addClass('on');
-  //   });
-
-  //   // mobile nav 닫기
-  //   $('#nav > div.header.mobile ul > li:eq(2) > a').on('click', function() {
-  //     $('#nav').removeClass('on');
-  //   });
-
-  // }
 }
 
 
 
 
+// pc로 이동했을때 모바일 흔적 지워
+function resetNavM() {
+  $('#nav').removeClass('on');
 
+}
 
+// mobile로 이동했을 때 pc 흔적 지워
+function resetNavPc() {
+  $('#nav > ul > li').removeAttr('class');
 
+}
 function calHeight() {
   var height = $('#footer .footer-inner').innerHeight();
 }
@@ -527,7 +491,7 @@ function resizeWindow() {
       console.log(height + ": 제발..");
       $('#footer button.footer-toggle-button').removeClass('on');
       $('#footer .more-footer').removeAttr('style');
-      if ($('#footer button.footer-toggle-button').hasClass('on') === true) {
+      if($('#footer button.footer-toggle-button').hasClass('on') === true) {
         $(this).css({
           'height': height + 'px'
         });
